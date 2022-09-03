e2aba88c03e626aadd34fe29485787d4275d86c6
Nehaishu24 <nehaom24@gmail.com>
2022-09-03 19:24:59 +0530

    new

---
+++ polymorphism/Shape.java
@@ -0,0 +1,23 @@
+package in.co.rays.polymorphism;
+
+public class Shape {
+ 
+	private int Color;
+	private int BorderWidth;
+	public int getColor() {
+	return Color;
+	}
+	public void setColor(int color) {
+	Color = color;
+	}
+	public int getBorderWidth() {
+	return BorderWidth;
+	}
+	public void setBorderWidth(int borderWidth) {
+	BorderWidth = borderWidth;
+	}
+	public double area() {
+	return 0;
+	}
+	}
+
